f1708824ab5a395e4ba3ad9f6257cc4f126dd137
andrew kim <akim@attinteractive.com>
2011-02-05 16:16:15 -0800

    Initial commit, blank rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatter::Application.config.secret_token = '5b6ceaf63a1eb4725c1399f5cadf7452d2d21715bb846a577051a7b5f5b69fd798f6431243177e92169aac4e70071b06c64e47ac4e50c1ccc88f1642ddd37a54'
